ca847f69378c2bff412481ed178a1d4c89c9a740
jun9771 <jun9771@gmail.com>
2025-05-31 01:49:55 +0900

    categoryapi

--- src/pages/product/ProductCardGrid.jsx
+++ src/pages/product/ProductCardGrid.jsx
@@ -62,69 +62,69 @@ const ProductCard = ({ image, discount, price, name, rating, reviews }) => {
   );
 };
 
-// 상품 데이터
-const products = [
-  {
-    id: 1,
-    name: "푸라롤팬던 원피스",
-    price: 17910,
-    originalPrice: 39800,
-    discount: 55,
-    image: "/images/product1.jpg",
-    rating: 4.8,
-    reviews: 1328
-  },
-  {
-    id: 2,
-    name: "플라워 패턴 원피스",
-    price: 25590,
-    originalPrice: 42650,
-    discount: 40,
-    image: "/images/product2.jpg",
-    rating: 4.6,
-    reviews: 1702
-  },
-  {
-    id: 3,
-    name: "[🖤]별자수 미니원피스",
-    price: 17910,
-    originalPrice: 39800,
-    discount: 55,
-    image: "/images/product3.jpg",
-    rating: 4.8,
-    reviews: 1328
-  },
-  {
-    id: 1,
-    name: "푸라롤팬던 원피스",
-    price: 17910,
-    originalPrice: 39800,
-    discount: 55,
-    image: "/images/product1.jpg",
-    rating: 4.8,
-    reviews: 1328
-  },
-  {
-    id: 2,
-    name: "플라워 패턴 원피스",
-    price: 25590,
-    originalPrice: 42650,
-    discount: 40,
-    image: "/images/product2.jpg",
-    rating: 4.6,
-    reviews: 1702
-  },
-  {
-    id: 3,
-    name: "[🖤]별자수 미니원피스",
-    price: 17910,
-    originalPrice: 39800,
-    discount: 55,
-    image: "/images/product3.jpg",
-    rating: 4.8,
-    reviews: 1328
-  }
-];
+// // 상품 데이터
+// const products = [
+//   {
+//     id: 1,
+//     name: "푸라롤팬던 원피스",
+//     price: 17910,
+//     originalPrice: 39800,
+//     discount: 55,
+//     image: "/images/product1.jpg",
+//     rating: 4.8,
+//     reviews: 1328
+//   },
+//   {
+//     id: 2,
+//     name: "플라워 패턴 원피스",
+//     price: 25590,
+//     originalPrice: 42650,
+//     discount: 40,
+//     image: "/images/product2.jpg",
+//     rating: 4.6,
+//     reviews: 1702
+//   },
+//   {
+//     id: 3,
+//     name: "[🖤]별자수 미니원피스",
+//     price: 17910,
+//     originalPrice: 39800,
+//     discount: 55,
+//     image: "/images/product3.jpg",
+//     rating: 4.8,
+//     reviews: 1328
+//   },
+//   {
+//     id: 1,
+//     name: "푸라롤팬던 원피스",
+//     price: 17910,
+//     originalPrice: 39800,
+//     discount: 55,
+//     image: "/images/product1.jpg",
+//     rating: 4.8,
+//     reviews: 1328
+//   },
+//   {
+//     id: 2,
+//     name: "플라워 패턴 원피스",
+//     price: 25590,
+//     originalPrice: 42650,
+//     discount: 40,
+//     image: "/images/product2.jpg",
+//     rating: 4.6,
+//     reviews: 1702
+//   },
+//   {
+//     id: 3,
+//     name: "[🖤]별자수 미니원피스",
+//     price: 17910,
+//     originalPrice: 39800,
+//     discount: 55,
+//     image: "/images/product3.jpg",
+//     rating: 4.8,
+//     reviews: 1328
+//   }
+// ];
 
 export default function ProductCardGrid() {
   return (
--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import ProductCardGrid from '../../components/product/ProductCardGrid';
-import { useProductdata } from '../../hooks/useProductdata' //임시로 product 화면에서 데이터 호출해서 확인하려고 넣음
+import { useProductdata } from '../../hooks/Product/useProductdata' //임시로 product 화면에서 데이터 호출해서 확인하려고 넣음
 
 const ProductPage = () => {
   const { products, loading, error } = useProductdata(); //임시로 product 화면에서 데이터 호출해서 확인하려고 넣음
